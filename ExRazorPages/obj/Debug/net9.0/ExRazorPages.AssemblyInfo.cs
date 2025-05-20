f7422f54c60b50da4d8e151f1c4be0989f1e3d76
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-05-20 00:42:41 -0600

    Segundo Commit Ejercicios de Razor

--- ExRazorPages/obj/Debug/net9.0/ExRazorPages.AssemblyInfo.cs
+++ ExRazorPages/obj/Debug/net9.0/ExRazorPages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExRazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530bb3941ce07abdaa04933f0f9487270ba216a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExRazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExRazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
